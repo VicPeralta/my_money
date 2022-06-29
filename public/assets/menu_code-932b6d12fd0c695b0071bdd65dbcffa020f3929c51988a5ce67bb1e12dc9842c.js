2a125310c3272cb2869d21ece70e54a60b1ec248
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-06-29 12:52:55 -0500

    Add precompiled assets

---
+++ public/assets/menu_code-932b6d12fd0c695b0071bdd65dbcffa020f3929c51988a5ce67bb1e12dc9842c.js
@@ -0,0 +1,30 @@
+console.log('Loading menu.js')
+function makeScrollable() {
+  const body = document.querySelector('body');
+  body.classList.remove('makeNotScrollable');
+  body.classList.add('makeScrollable');
+}
+
+function makeNotScrollable() {
+  const body = document.querySelector('body');
+  body.classList.remove('makeScrollable');
+  body.classList.add('makeNotScrollable');
+}
+
+function closeMenu() {
+  const menu = document.querySelector('.menu-options');
+  menu.classList.remove('visible');
+  menu.classList.add('invisible');
+  makeScrollable();
+}
+
+document.querySelector('.hamburger-button').addEventListener('click', () => {
+  const menu = document.querySelector('.menu-options');
+  menu.classList.remove('invisible');
+  menu.classList.add('visible');
+  makeNotScrollable();
+});
+
+document.querySelector('#close-btn').addEventListener('click', () => {
+  closeMenu();
+});
